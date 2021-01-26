948985f79bea73a49ebef4a7c9655a582f3b0d43
Lion Kortlepel <development@kortlepel.com>
2021-01-26 15:08:03 +0100

    add demo gif

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ int main() {
     while (true) {
         if (com.has_command()) {
             auto command = com.get_command();
-            com.write("got " + command);
+            com.write("got command: " + command);
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        com.write("hey hey whats goin on my dude");
+        com.write("this is a message written with com.write");
     }
 }
