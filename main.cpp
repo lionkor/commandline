aa47fa97ae2582fa6493bb11815f375f8c4034d7
20dka <doni@doni.hu>
2022-03-15 01:44:32 +0100

    Add tab autocomplete (fix #7)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ int main(int argc, char** argv) {
     }
     com.enable_history();
     com.set_history_limit(5);
+
+    com.on_autocomplete = [](Commandline& com, std::string stub) {
+        //com.write(stub);
+        return std::vector<std::string>{"yo", "uwu"};
+    };
+
     while (true) {
         if (com.has_command()) {
             auto command = com.get_command();
@@ -20,6 +26,6 @@ int main(int argc, char** argv) {
         // usually, instead of writing a message here, a message would
         // be written as the result of some internal program event.
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        com.write("this is a message written with com.write");
+        //com.write("this is a message written with com.write");
     }
 }
